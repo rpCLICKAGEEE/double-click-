9d89ac83d365c2ffae414911a8e9fc25b1e834da
rpCLICKAGEEE <166063245+rpCLICKAGEEE@users.noreply.github.com>
2024-04-16 22:42:19 +0200

    authentification (3 users)+ randomisation

--- main.cpp
+++ main.cpp
@@ -4,55 +4,39 @@
 #include <random>
 #include <chrono>
 
+bool CheckCredentials(const std::string& username, const std::string& password) {
+    return ((username == "rpCLICKAGEE" && password == "rp") ||
+        (username == "rpCLICKAGEE2" && password == "rp2") ||
+        (username == "rpCLICKAGEE3" && password == "rp3"));
+}
+
 void StartDoubleClick(bool& doubleClickEnabled, int& cps) {
     doubleClickEnabled = true;
     std::cout << "Double clic active avec " << cps << " CPS." << std::endl;
-}
-
-void SetCPS(int& cps) {
-    std::string input;
-    while (true) {
-        std::cout << "Veuillez entrer le nombre de clics par seconde (CPS) : ";
-        std::cin >> input;
-
-        try {
-            cps = std::stoi(input);
-            if (cps > 0) {
-                break;
-            }
-            else {
-                std::cout << "Veuillez entrer un nombre de CPS valide (supérieur à 0)." << std::endl;
-            }
-        }
-        catch (std::invalid_argument&) {
-            std::cout << "Veuillez entrer un nombre valide." << std::endl;
-        }
-    }
 }
 
 int main() {
     std::string username, password;
     bool doubleClickEnabled = false;
     int cps = 20; // Défaut : 20 clics par seconde
-    int delay = 1000 / cps; 
-
-    SetConsoleTitleA("");
-
+    int delay = 1000 / cps;
+    
     std::cout << "Entrer votre nom d'utilisateur : ";
     std::cin >> username;
     std::cout << "Entrer votre mot de passe : ";
     std::cin >> password;
 
-    if (username == "rpCLICKAGEE" && password == "rp") {
+    if (CheckCredentials(username, password)) {
+        std::cout << "Authentification reussie." << std::endl;
         StartDoubleClick(doubleClickEnabled, cps);
     }
     else {
         std::cout << "Nom d'utilisateur ou mot de passe incorrect." << std::endl;
-        return 0;
+        return 0; // 
     }
-    std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
-    std::uniform_int_distribution<int> distribution(-20, 20); 
 
+    std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
+    std::uniform_int_distribution<int> distribution(-20, 20); // Distribution uniforme de -20 à 20 millisecondes
     while (true) {
         if (doubleClickEnabled && (GetAsyncKeyState(VK_LBUTTON) & 0x8000)) {
             // Envoyer deux clics successifs
@@ -64,9 +48,12 @@ int main() {
 
             SendInput(1, &input[0], sizeof(INPUT));
             Sleep(delay + distribution(generator));
+
+            // Envoyer le deuxième clic
             SendInput(1, &input[1], sizeof(INPUT));
         }
         Sleep(10);
     }
+
     return 0;
 }
